07f758d4eed051a7a64e50c76ab1a788494d05bf
Kel Sergej <112585930+KS7773@users.noreply.github.com>
2022-09-10 18:03:51 +0200

    Create JavaBasic1Hw.java

---
+++ JavaBasic1Hw.java
@@ -0,0 +1,37 @@
+/ **
+ * Java Basic. Home work #!1
+ * @autor Sergej Kel
+ *@ 5.9.2022
+ @date 9.9.2022
+class JavaBasic1Hw {
+    public static void main(String[] args) {
+        // taks 1
+        int a=2
+        int a=4
+        //int sum = a*b
+
+        // task 2
+        int a=3
+        int b=5
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+
+        // task 3
+        int[] a = new int [5];
+        a[0] =2
+        a[1] =5
+        a[2] =6
+        a[3] =7
+        a[4] =8
+
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+        System.out.println(a[3]);
+        System.out.println(a[4]);
+        }
+}
+
+
